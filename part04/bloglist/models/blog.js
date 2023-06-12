f105744499183b276e398702d07cd3573addc4c5
alexpergar <aleperaltagar@gmail.com>
2023-06-12 17:23:16 +0200

    exercises 4.1 & 4.2

--- part04/bloglist/models/blog.js
+++ part04/bloglist/models/blog.js
@@ -1,8 +1,6 @@
 const config = require('../utils/config')
 const mongoose = require('mongoose')
 
-mongoose.connect(config.MONGODB_URI)
-
 const blogSchema = new mongoose.Schema({
     title: String,
     author: String,
