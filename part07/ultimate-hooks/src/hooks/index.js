2d5c524b1e752c8c55bc65710d25534c0d52e09e
alexpergar <aleperaltagar@gmail.com>
2023-09-03 17:55:51 +0200

    exercise 7.8

---
+++ part07/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data)
+  }
+
+  useEffect(() => {
+    getAll()
+  }, [])
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources([...resources, response.data])
+  }
+
+  const service = {
+    create
+  }
+
+  return [
+    resources, service
+  ]
+}
+
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
